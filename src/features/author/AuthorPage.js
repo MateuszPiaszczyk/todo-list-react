fe140996d7cab6f10834aae6cf1aec3acb4b84b3
Mateusz Piaszczyk <mateuszpiaszczyk91@gmail.com>
2023-04-06 15:18:27 +0200

    repair Image Visibiled

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -2,8 +2,8 @@ import Container from "../../common/Container";
 import Footer from "../../common/Footer";
 import Header from "../../common/Header";
 import Section from "../../common/Section";
-import { Image } from "./styled";
-
+import { Description, Image, Strong, Title, Wrapper, Youcode } from "./styled";
+import image from "./assets/Mateusz.jpg";
 
 export default () => (
   <Container>
@@ -12,12 +12,26 @@ export default () => (
       title="Mateusz Piaszczyk"
       body={
         <>
-          <Image alt="Mateusz Piaszczyk" />
-          <p>
-            Nazywam się Mateusz Piaszczyk i jestem{" "}
-            <strong>grafikiem komputerowym</strong>, a także mam nadzieje
-            przyszłym <strong>front-end developerem</strong> 😊.
-          </p>
+          <Wrapper>
+            <Image src={image} alt="Mateusz Piaszczyk" />
+            <Description>
+              <Title>Opis/Doświadczenie:</Title>
+              Jestem <Strong>grafikiem komputerowym</Strong>, a także mam
+              nadzieje przyszłym <Strong>front-end developerem</Strong> 😊.
+              Swoje <Strong>doświadczenie w projektowaniu</Strong> zdobyłem
+              dzięki współpracy takimi podmiotami jak: <b>studia filmowe</b>,{" "}
+              <b>fundacje</b>, <b>sklepy</b>,{" "}
+              <b>salon wirtualnej rzeczywistości</b>{" "}
+              <i>
+                (w którym pracowałem w latach 2018-2020 jako grafik /
+                webmaster).
+              </i>
+              <br />
+              <Title>Obecnie:</Title>
+              Nauke programowania w React.js, pod okiem ekipy{" "}
+              <Youcode>Youcode Team</Youcode> zacząłem na początku roku 2023.
+            </Description>
+          </Wrapper>
         </>
       }
     />
--- src/features/author/styled.js
+++ src/features/author/styled.js
@@ -1,7 +1,32 @@
 import styled from "styled-components";
 
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-gap: 20px;
+`;
+
+export const Description = styled.div`
+  line-height: 1.5;
+`;
+
+export const Strong = styled.strong`
+  font-weight: 700;
+`;
+
 export const Image = styled.img`
-    background-image: url("./assets/Mateusz.jpg");
-    width: 100%;
+    display: grid:
+    border-radius: 50%;
     max-width: 300px;
+`;
+
+export const Title = styled.h2`
+    color: ${({ theme }) => theme.color.teal};
+    padding-bottom: 10px;
+    margin: 0;   
+`;
+
+export const Youcode = styled.span`
+    color: ${({ theme }) => theme.color.teal};
+    
 `;
